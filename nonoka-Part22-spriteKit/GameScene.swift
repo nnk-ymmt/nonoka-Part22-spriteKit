eb3d69abdcb892e321fff69fa5afcf40bb869d68
nnk-ymmt <garden.n1234567890@gmail.com>
2021-03-10 23:37:29 +0900

    更新

---
+++ nonoka-Part22-spriteKit/GameScene.swift
@@ -0,0 +1,72 @@
+//
+//  GameScene.swift
+//  nonoka-Part22-spriteKit
+//
+//  Created by 山本ののか on 2021/03/06.
+//
+
+import SpriteKit
+import GameplayKit
+
+final class GameScene: SKScene {
+
+    private var diceNode = SKSpriteNode(imageNamed: "dice1")
+
+    override func didMove(to view: SKView) {
+        let w = (self.size.width + self.size.height) * 0.05
+        diceNode.position = CGPoint(x: w, y: w)
+        self.addChild(diceNode)
+    }
+    
+    
+//    func touchDown(atPoint pos : CGPoint) {
+//        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
+//            n.position = pos
+//            n.strokeColor = SKColor.green
+//            self.addChild(n)
+//        }
+//    }
+    
+//    func touchMoved(toPoint pos : CGPoint) {
+//        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
+//            n.position = pos
+//            n.strokeColor = SKColor.blue
+//            self.addChild(n)
+//        }
+//    }
+    
+//    func touchUp(atPoint pos : CGPoint) {
+//        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
+//            n.position = pos
+//            n.strokeColor = SKColor.red
+//            self.addChild(n)
+//        }
+//    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+//        if let label = self.label {
+//            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+//        }
+        
+        for touch in touches {
+            
+        }
+    }
+    
+//    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+//        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+//    }
+//
+//    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+//        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+//    }
+//
+//    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+//        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+//    }
+    
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+    }
+}
